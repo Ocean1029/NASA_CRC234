e3d637983147a6ea1e8fcc09fe95fa63aa029b86
peaceful_cake <samuel930514@gmail.com>
2024-10-06 22:34:17 +0800

    更改單位

--- js/gameUI.js
+++ js/gameUI.js
@@ -12,15 +12,16 @@ const elecSumElement = document.getElementById('elec-sum');
 const supportElement = document.getElementById('support');
 const fossilCountElement = document.getElementById('fossil-count');
 
+
 // 更新 UI 資訊的函式
 export function updateInfo() {
     roundElement.textContent = gameState.round;
     financeElement.textContent = gameState.finance;
     greenElement.textContent = gameState.green_installed_capacity;
     fossilElement.textContent = gameState.fossil_installed_capacity;
-    greenProductionElement.textContent = gameState.green_installed_capacity / 0.2 * gameState.randomValue;
-    fossilProductionElement.textContent = gameState.fossil_installed_capacity * 8760 * 0.9;
-    elecSumElement.textContent = gameState.fossil_electricity_production + gameState.green_electricity_production;
+    greenProductionElement.textContent = Math.round((gameState.green_installed_capacity / 0.2 * gameState.randomValue) / 1000);
+    fossilProductionElement.textContent = Math.round((gameState.fossil_installed_capacity * 8760 * 0.9) / 1000);
+    elecSumElement.textContent = Math.round((gameState.green_installed_capacity / 0.2 * gameState.randomValue + gameState.fossil_installed_capacity * 8760 * 0.9) / 1000);
     supportElement.textContent = `${gameState.support}%`;
     fossilCountElement.textContent = gameState.fossilCount;
 }
